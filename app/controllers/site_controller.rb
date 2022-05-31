9b508774284a7a8163f9e135954c36d6d3ad29e9
KevinGolfimmo <71709920+KevinGolfimmo@users.noreply.github.com>
2022-05-31 18:02:09 +0200

    send message turbo ok

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,8 @@ class SiteController < ApplicationController
   def increment
     @counter = (params[:counter]&.to_i || 0) + 1
   end
+
+  def index; end
+  
+  def turbo_frame_test; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end 
+
+
   get 'site/increment', as: :increment
   root 'site#index'
+
+  get 'site/something' => 'site#something'
+
+
+
 
 end
